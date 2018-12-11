6dce6e003799936ce2fccf65804800f118d8ac91
zachpnnc <zachpnnc@gmail.com>
2018-12-10 20:18:49 -0500

    Added images

---
+++ app/src/main/java/com/bignerdranch/android/pocketturchin/ArtPagerActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.pocketturchin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ArtPagerActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_pager);
+    }
+}
